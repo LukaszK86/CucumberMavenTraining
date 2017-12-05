1d1c85a77b28c1163bba07ff31f9cfaa59e7a0cd
Lukasz <lucas.kaczmarczyk@gmail.com>
2017-12-05 08:45:51 +0100

    Create the project Maven Cucumber

---
+++ src/test/java/Usage.java
@@ -0,0 +1,44 @@
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import net.masterthought.cucumber.Reportable;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Usage {
+
+    public static void main(String[] args) {
+
+        File reportOutputDirectory = new File("target");
+        List<String> jsonFiles = new ArrayList<>();
+        jsonFiles.add("cucumber.json");
+        //jsonFiles.add("cucumber-report-2.json");
+
+        String buildNumber = "1";
+        String projectName = "SeleniumWithCucumber";
+        boolean runWithJenkins = false;
+        boolean parallelTesting = false;
+
+        Configuration configuration = new Configuration(reportOutputDirectory, projectName);
+// optional configuration
+        configuration.setParallelTesting(parallelTesting);
+        configuration.setRunWithJenkins(runWithJenkins);
+        configuration.setBuildNumber(buildNumber);
+// addidtional metadata presented on main page
+        configuration.addClassifications("Platform", "Windows");
+        configuration.addClassifications("Browser", "Firefox");
+        configuration.addClassifications("Branch", "release/1.0");
+
+        // optionally add metadata presented on main page via properties file
+        //List<String> classificationFiles = new ArrayList<>();
+//        classificationFiles.add("properties-1.properties");
+//        classificationFiles.add("properties-2.properties");
+//        configuration.addClassificationFiles(classificationFiles);
+
+        ReportBuilder reportBuilder = new ReportBuilder(jsonFiles, configuration);
+        Reportable result = reportBuilder.generateReports();
+// and here validate 'result' to decide what to do
+// if report has failed features, undefined steps etc
+    }
+}
---
+++ src/test/java/steps/Hook.java
@@ -0,0 +1,38 @@
+package steps;
+
+import Base.BaseUtil;
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Hook extends BaseUtil {
+
+
+    private BaseUtil base;
+
+    public Hook(BaseUtil base) {
+        this.base = base;
+    }
+
+    @Before
+    public void InitializeTest() {
+
+        System.out.println("Opening the browser : Firefox");
+
+        System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
+        base.Driver = new ChromeDriver();
+    }
+
+
+    @After
+    public void TearDownTest(Scenario scenario) {
+        if (scenario.isFailed()) {
+            //Take screenshot logic goes here
+            System.out.println(scenario.getName());
+        }
+        base.Driver.quit();
+        System.out.println("Closing the browser : MOCK");
+    }
+
+}
